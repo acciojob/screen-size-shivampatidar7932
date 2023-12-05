caf9888308ee83fd8a90940eed6c0560ec2c8f0e
Shivam Patidar <125869590+shivampatidar7932@users.noreply.github.com>
2023-12-05 12:36:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your JS code here. If required.
+ const sizeInfo = document.getElementById("sizeInfo");
+        const screenSizeElement = document.getElementById("screenSize");
+
+        function updateScreenSize() {
+            const width = window.innerWidth;
+            const height = window.innerHeight;
+
+            screenSizeElement.textContent = `Width: ${width} and Height: ${height}`;
+        }
+
+        // Initial update
+        updateScreenSize();
+
+        // Update on window resize
+        window.addEventListener("resize", updateScreenSize);
